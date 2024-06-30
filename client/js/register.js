ed4ab3a4c6143e3e5c2f4f62d8728e31f76c7635
LuanNguyen <thienluan678@gmail.com>
2024-07-01 01:40:14 +0700

    linked page, validate

--- client/js/register.js
+++ client/js/register.js
@@ -2,17 +2,29 @@ function handleRegister() {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
 
     axios.post('http://localhost:5000/api/auth/register', { username: username, email: email, password: password })
         .then((response) => {
-            if (response.data.error) {
-                alert(response.data.error);
+            if (response.status === 200) {
+                alert('Successfully registered');
+                window.location.href = '/client/login.html';
             } else {
-                alert('Register successful');
-                // window.location.href = '/login';
+                 // Lấy thông điệp lỗi từ JSON
+                 const error = data.error || 'An unknown error occurred';
+                 alert(error);
             }
         })
         .catch((error) => {
+            // Hiển thị thông báo lỗi từ server hoặc một lỗi mặc định
+            const message = error.response && error.response.data ? error.response.data.error : 'An unknown error occurred';
+            console.error(message); // Vẫn giữ log cho developer
+            if(errorMessage) {
+                errorMessage.textContent = message // Hiển thị lỗi trên UI
+                errorMessage.style.display = 'block';
+            } else {
+                alert(message); // Nếu không có phần tử errorMessage, sử dụng alert
+            }
             console.error(error);
         }); 
 }
--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,4 +1,5 @@
 const userModel = require('../Models/UserModel');
+const bcrypt = require('bcrypt');
 
 const register = async (req, res) => {
     try {
@@ -6,13 +7,18 @@ const register = async (req, res) => {
         await userModel.create({
             username,
             email,
-            password,
+            password: bcrypt.hashSync(password, 10),
             role: 'regular'
         })
         return res.status(200).send('register user success')
 
     } catch (error) {
         console.error(error)
+        if(error.code === 11000) {
+            return res.status(400).json({error: 'Username or email already exists'});
+        } else {
+            return res.status(500).json({error: 'Internal Server Error'});
+        }
     }
 }
 
